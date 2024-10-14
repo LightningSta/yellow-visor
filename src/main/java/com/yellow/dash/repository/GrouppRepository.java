c1b2a7505e8de0b240e468c75d93d7467efe5c5f
LightningSta <92336589+LightningSta@users.noreply.github.com>
2024-10-15 04:08:59 +0900

    Delete src directory

--- src/main/java/com/yellow/dash/repository/GrouppRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yellow.dash.repository;
-
-import com.yellow.dash.model.Groupp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GrouppRepository extends JpaRepository<Groupp, Integer> {
-}
